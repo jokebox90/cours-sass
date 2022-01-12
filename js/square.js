45a48d4bf63b77d0c683af9a8d10ea7eccb7607b
Jonathan Vagnier <adm.jvagnier@gmail.com>
2022-01-12 16:54:27 +0100

    Déplace le carré dans toutes les directions

--- js/square.js
+++ js/square.js
@@ -2,14 +2,35 @@ let playground = document.querySelector('body');
 
 playground.addEventListener('keydown', function(ev){
     let square = document.getElementById('square');
-    if (ev.key == "ArrowDown") {
-        square.parentElement.style.alignItems = 'flex-end';
+    switch (ev.key) {
+        case "ArrowDown":
+            square.parentElement.style.alignItems = 'flex-end';
+
+            break;
+
+        case "ArrowUp":
+            square.parentElement.style.alignItems = 'flex-start';
+
+            break;
+
+        case "ArrowLeft":
+            square.parentElement.style.justifyContent = 'flex-start';
+
+            break;
+
+        case "ArrowRight":
+            square.parentElement.style.justifyContent = 'flex-end';
+
+            break;
+
+        default:
+            break;
     }
 });
 
 playground.addEventListener('keyup', function(ev){
     let square = document.getElementById('square');
-    if (ev.key == "ArrowDown") {
-        square.parentElement.style.alignItems = 'center';
-    }
+
+    square.parentElement.style.alignItems = 'center';
+    square.parentElement.style.justifyContent = 'center';
 });
